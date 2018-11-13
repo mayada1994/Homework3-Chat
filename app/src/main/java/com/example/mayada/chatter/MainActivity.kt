e7ff5cc3e067f9fc66158788e03345c1b0893952
Mayada Al-Sawah <mayada.m.alsawah@gmail.com>
2018-11-13 18:17:54 +0200

    View one message

--- app/src/main/java/com/example/mayada/chatter/MainActivity.kt
+++ app/src/main/java/com/example/mayada/chatter/MainActivity.kt
@@ -3,35 +3,30 @@ package com.example.mayada.chatter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
+import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
-    private val messages:ArrayList<Message> = ArrayList()
+    private val messages: ArrayList<Message> = ArrayList()
     private val messageAdapter: MessageAdapter = MessageAdapter(messages)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var editMessage = findViewById<EditText>(R.id.edit_message)
-        var radioGroup = findViewById<RadioGroup>(R.id.radio_group)
-        var messagesRecycler = findViewById<RecyclerView>(R.id.messages_recycler_view)
-        messagesRecycler.layoutManager = LinearLayoutManager(this)
-        messagesRecycler.adapter = this.messageAdapter
-        val okButton: Button = findViewById(R.id.buttonOK)
-        okButton.setOnClickListener {
-            val checkedRadioButton = radioGroup.checkedRadioButtonId
-            val currentText = editMessage.text.toString()
-            val currentUser = when (checkedRadioButton) {
+
+        messages_recycler_view.layoutManager = LinearLayoutManager(this)
+        messages_recycler_view.adapter = messageAdapter
+        buttonOK.setOnClickListener {
+            radio_group.checkedRadioButtonId
+            val currentText = edit_message.text.toString()
+            val currentUser = when (radio_group.checkedRadioButtonId) {
                 R.id.user1Select -> 1
                 R.id.user2Select -> 2
                 else -> throw IllegalArgumentException()
             }
             messageAdapter.postMessage(Message(currentUser, currentText))
-            editMessage.text.clear()
+            messageAdapter.notifyItemRangeChanged(0, messageAdapter.getItemCount());
+            edit_message.text.clear()
         }
     }
 }
--- app/src/main/java/com/example/mayada/chatter/MessageAdapter.kt
+++ app/src/main/java/com/example/mayada/chatter/MessageAdapter.kt
@@ -7,19 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 class MessageAdapter(var messages: ArrayList<Message>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
-
-    var user1Messages:Int = 0
-    var user2Messages:Int = 0
+    var pos:Int = 0
     companion object {
         const val TYPE_FIRST_USER = 1
         const val TYPE_SECOND_USER = 2
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val layoutResource: Int = when (getItemViewType(viewType)) {
-            TYPE_FIRST_USER -> R.layout.first_user_message
-            TYPE_SECOND_USER -> R.layout.second_user_message
-            else -> throw IllegalArgumentException()
+        val layoutResource: Int
+        if (viewType == TYPE_FIRST_USER) {
+            layoutResource = R.layout.first_user_message
+        } else {
+            layoutResource = R.layout.second_user_message
         }
 
         val view: View = LayoutInflater.from(viewGroup.context).inflate(layoutResource, viewGroup, false)
@@ -27,24 +26,15 @@ class MessageAdapter(var messages: ArrayList<Message>) : RecyclerView.Adapter<Me
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        val message = messages[position]
+        var message: Message = messages[pos]
+        pos++
         viewHolder.messageText.text = message.messageText
-
-        viewHolder.firstUserMessages.setText(user1Messages)
-        viewHolder.secondUserMessages.setText(user2Messages)
-
-        when(message.messageUser)
-        {
-            1 -> user1Messages++
-            2 -> user2Messages++
-        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        val type = when (messages[position].messageUser) {
+        val type = when (messages[pos].messageUser) {
             1 -> TYPE_FIRST_USER
-            2 -> TYPE_SECOND_USER
-            else -> throw IllegalArgumentException()
+            else -> TYPE_SECOND_USER
         }
         return type
     }
@@ -55,12 +45,13 @@ class MessageAdapter(var messages: ArrayList<Message>) : RecyclerView.Adapter<Me
 
     fun postMessage(message: Message) {
         messages.add(message)
+        notifyDataSetChanged()
     }
+
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val messageText = itemView.findViewById<TextView>(R.id.message_text)
-        val firstUserMessages = itemView.findViewById<TextView>(R.id.first_user_count)
-        val secondUserMessages = itemView.findViewById<TextView>(R.id.second_user_count)
     }
+
 }
 
